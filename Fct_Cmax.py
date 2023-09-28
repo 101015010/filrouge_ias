1e6b6efa7836e52bbfc63fcd9c26f641dbe72b69
Moortgatt <145100562+Moortgatt@users.noreply.github.com>
2023-09-28 14:34:40 +0200

    Add files via upload

---
+++ Fct_Cmax.py
@@ -0,0 +1,140 @@
+###Définition des représentations
+import numpy as np
+import random as rd
+import matplotlib.pyplot as pl
+import copy
+
+n_patient = 100
+n_SSPI_cap_pre = 3
+n_SSPI_cap_post =3
+n_salle_op = 4
+n_medecin = 5
+n_anestesiste = 8
+n_brancardier = 2
+n_phase = 4
+
+limite_de_phase=[n_brancardier, [n_SSPI_cap_pre,n_anestesiste], [n_salle_op,n_medecin,n_anestesiste],[n_SSPI_cap_post,n_anestesiste]]
+#Le medecin doit être lié au patient
+
+
+
+def gen_temps_restant(liste_ope):
+    res = copy.deepcopy(liste_ope)
+    return res
+
+
+def peutetreprisencharge(temps_restant,solution,phase,liste_en_cours):
+    for i in solution[phase]:
+        if temps_restant[i][phase] != 0:
+            if i not in liste_en_cours:
+                etape_suivante = True
+                for etape in range(0,phase):
+                    if temps_restant[i][etape]!=0:
+                        etape_suivante = False
+                if etape_suivante:
+                    return i
+    return None
+
+def gen_liste_ope(n_patient):
+    return [[rd.randint(5,10),rd.randint(10,30),rd.randint(40,120),rd.randint(10,30)] for i in range(n_patient)]
+
+
+def gen_solution_initiale():
+    solution = [list(range(n_patient)) for _ in range(4)]
+    for s in solution:
+        rd.shuffle(s)
+    return solution
+
+
+def Cmax(solution,liste_ope,SSPI_cap_pre = 3,SSPI_cap_post =3,salle_op = 4,medecin = 5,anestesiste = 8,brancardier = 2):
+    n_SSPI_cap_pre = SSPI_cap_pre
+    n_SSPI_cap_post =SSPI_cap_post
+    n_salle_op = salle_op
+    n_medecin = medecin
+    n_anestesiste = anestesiste
+    n_brancardier = brancardier
+    temps_restant = gen_temps_restant(liste_ope)
+    fin = False
+    t = 0
+    en_cours_brancardage = []
+    en_cours_SSPI_pre = []
+    en_cours_operation = []
+    en_cours_SSPI_post = []
+    while fin == False:
+        #Prise en charge
+        if n_brancardier != 0:
+            patient = peutetreprisencharge(temps_restant,solution,0,en_cours_brancardage)
+            if patient != None:
+                en_cours_brancardage.append(patient)
+                n_brancardier -=1
+
+        if (n_SSPI_cap_pre != 0) and (n_anestesiste != 0):
+            patient = peutetreprisencharge(temps_restant,solution,1,en_cours_SSPI_pre)
+            if patient != None:
+                en_cours_SSPI_pre.append(patient)
+                n_SSPI_cap_pre -=1
+                n_anestesiste -=1
+
+        if (n_medecin != 0) and (n_anestesiste !=0) and (n_salle_op != 0):
+            patient = peutetreprisencharge(temps_restant,solution,2,en_cours_operation)
+            if patient != None:
+                en_cours_operation.append(patient)
+                n_medecin -= 1
+                n_anestesiste -= 1
+                n_salle_op -=1
+
+        if (n_SSPI_cap_post != 0) and (n_anestesiste != 0):
+            patient = peutetreprisencharge(temps_restant,solution,3,en_cours_SSPI_post)
+            if patient != None:
+                en_cours_SSPI_post.append(patient)
+                n_SSPI_cap_post -=1
+                n_anestesiste -=1
+        #écoulement du temps
+        for i in en_cours_brancardage:
+            temps_restant[i][0] -= 1
+            if temps_restant[i][0] == 0:
+                n_brancardier += 1
+        en_cours_brancardage = [i for i in en_cours_brancardage if temps_restant[i][0] > 0]
+        for i in en_cours_SSPI_pre:
+            temps_restant[i][1] -= 1
+            if temps_restant[i][1] == 0:
+                n_SSPI_cap_pre += 1
+                n_anestesiste += 1
+        en_cours_SSPI_pre = [i for i in en_cours_SSPI_pre if temps_restant[i][1] > 0]
+
+
+        for i in en_cours_operation:
+            temps_restant[i][2] -= 1
+            if temps_restant[i][2] == 0:
+                n_medecin += 1
+                n_anestesiste += 1
+                n_salle_op += 1
+        en_cours_operation = [i for i in en_cours_operation if temps_restant[i][2] > 0]
+
+
+        for i in en_cours_SSPI_post:
+            temps_restant[i][3] -= 1
+            if temps_restant[i][3] == 0:
+                n_SSPI_cap_post += 1
+                n_anestesiste += 1
+        en_cours_SSPI_post = [i for i in en_cours_SSPI_post if temps_restant[i][3] > 0]
+
+        t+=1
+        fin = True
+        for i in range(len(temps_restant)):
+            if  temps_restant[i][3] != 0:
+                fin = False
+    print(temps_restant)
+    return t
+
+
+
+
+
+
+
+
+
+
+
+
